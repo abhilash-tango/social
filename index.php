5a66d4bad072aaaded6a7c53c5ede58bd42c6b13
= <=abhilash.tango@gmail.com>
2014-06-04 18:20:37 +0530

    o

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+<link href="bs/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="row">
+  <div class="col-xs-2 col-md-2">
+    <a href="#" class="thumbnail">
+      <img data-src="holder.js/100%x180" alt="..." src ="logo.jpg">
+    </a>
+  </div>
+  <div class="panel panel-default text-center">
+  <div class="panel-body">
+    Panel content
+  </div>
+ 
+</div>
+</div>
+
+</body>
+</html>
